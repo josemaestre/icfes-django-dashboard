79f6eaf27ff4ba7bb72b079b5439fd41acc39eb1
Jose Gregorio <josemaestre@gmail.com>
2026-01-18 09:12:32 -0500

    fix: Add missing allauth AccountMiddleware
    - Added allauth.account.middleware.AccountMiddleware
    - Required by django-allauth
    - Fixes worker boot error

--- reback/config/settings/railway.py
+++ reback/config/settings/railway.py
@@ -26,6 +26,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "allauth.account.middleware.AccountMiddleware",  # Required by allauth
     "reback.users.middleware.AutoCreateAdminMiddleware",  # Auto-create admin
 ]
 
