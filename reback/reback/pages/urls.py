23f9fb4a0f8ee45bd949c0979c04ccff8a75d1b5
Jose Gregorio <josemaestre@gmail.com>
2026-02-07 18:54:35 -0500

    feat: make landing page the homepage and move dashboard to /dashboard/

--- reback/reback/pages/urls.py
+++ reback/reback/pages/urls.py
@@ -8,7 +8,9 @@ app_name = 'pages'
 urlpatterns = [
     path('landing/', landing_page_view, name="landing"),
     path('pricing/', lambda request: render(request, 'pages/pricing.html'), name="pricing"),
-    path('', root_page_view, name="dashboard"),
+    path('', landing_page_view, name="home"),
+    path('dashboard/', root_page_view, name="dashboard"),
+    path('app/', root_page_view, name="app"),
     path('<str:template_name>/', dynamic_pages_view, name='dynamic_pages')
 ]
 
